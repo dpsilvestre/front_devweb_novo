154d4fd12c7a004aeee76ffeec8020e45739fd85
dpsilvestre <dp2132332@gmail.com>
2024-07-26 11:48:25 -0300

    api do vue

---
+++ src/views/coresView.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+
+export default class CoresApi {
+    async buscarTodasAsCores() {
+        const { data } = await axios.get("/cores/");
+        return data.results;
+    }
+    async adicionarCor(cor) {
+        const { data } = await axios.post("/cores/", cor);
+        return data.results;
+    }
+    async atualizarCor(cor) {
+        const { data } = await axios.put(`/cores/${cor.id}/`, cor);
+        return data.results;
+    }
+    async excluirCor(id) {
+        const { data } = await axios.delete(`/cores/${id}/`);
+        return data.results;
+    }
+}
+</script>
